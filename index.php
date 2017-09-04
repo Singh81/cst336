738342e8aae57362090b9ba722b7583df6e3a286
Ravinder Singh <ambani.verma@gmail.com>
2017-09-04 22:12:23 +0000

    Changed to html footer

--- index.php
+++ index.php
@@ -70,11 +70,18 @@ in html
         <!-- This is the footer -->
         <!-- The footer goes inside the body but not always -->
         <footer>
-          <?php
-
-         include_once('footer.php');
-
-         ?>
+          <hr>
+            <div class="left"></div>
+            <div class="middle">
+                Course Name CST336. 2017&copy; Singh 
+                <br/>
+                
+                <strong> Disclaimer:</strong> The information in this ewbpage is fictitous.
+                <br />
+            </div>
+            <div class="right">
+                <img id="logo" src="img/csumb.png" alt="Picture of csumb logo" />
+            </div>
         </footer>
         <!-- closing footer -->
         
