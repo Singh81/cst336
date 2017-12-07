85971434f259558252290e8b9431a8864274c2b6
Ravinder Singh <ambani.verma@gmail.com>
2017-12-07 03:19:42 +0000

    final

--- 412_webAssest/database.php
+++ 412_webAssest/database.php
@@ -2,11 +2,12 @@
 //function to connect to a mySQL db server
 
 function getDBConnection(){
-     $host = "us-cdbr-iron-east-05.cleardb.net";
-     $user = "b724345846f34e";
-    $pass = "958d7a51";
-    $dbname = "heroku_90e40dfb232c3c6";
     
+    
+    $host = "us-cdbr-iron-east-05.cleardb.net";
+     $username = "b253634d031b02";
+     $password = "a3d6690d";
+     $dbname="heroku_14ce4fd1a99cddb";
     //make connection
     // $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     // $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,8 +16,8 @@ function getDBConnection(){
     //b724345846f34e:958d7a51@us-cdbr-iron-east-05.cleardb.net/heroku_90e40dfb232c3c6?reconnect=true
     
    // $host = "localhost";
-     //$username = "root";
-   //  $password = "cst336";
+    // $username = "root";
+    // $password = "cst336";
     // $dbname="assest";
 
 // Create connection
